48cd7b34fc07a8a601a7998b221b796fd3612a8f
Yousef Kotp <yousefkotp@outlook.com>
2022-05-15 15:52:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,58 @@
 import DataStructure as ds
-tree= ds.RedBlackTree()
+
+tree = ds.RedBlackTree()  # initialize RB-tree
+DICTIONARY_NAME = "EN-US-Dictionary"
+
+
 def readFile(fileName):
     file = open(fileName, "r")
     for i in file:
-        tree.insert(i.rstrip('\n'))
+        if not tree.search(i.rstrip('\n')):
+            tree.insert(i.rstrip('\n'))
     file.close()
+
+
 while True:
     print("What do you want to do?")
-    option = int(input("1- Load Dictionary\t2- Print size of the Dictionary\n3- Insert Word\t4- Look-up a Word\n5-Print Tree Height\t6- Exit\n"))
-    if option ==1:
-        readFile("EN-US-Dictionary.txt")
-    elif option ==2:
-        print(tree.number_of_nodes)
-    elif option ==3:
-        s = str(input("Enter the word you want to insert: "))
-        if tree.search(s):
-            print("ERROR: Word already in the dictionary!")
-        else:
-            tree.insert(s)
-            print("Inserted Successfully")
-    elif option==4:
-        s = str(input("Enter the word you want to look-up: "))
-        if tree.search(s):
-            print("YES")
-        else:
-            print("NO")
-    elif option ==5:
-        print(tree.heightOfTree(tree.root,0))
-    else:
-        print("Thank you for using our application! :)")
-        break;
+    option = input(
+        "1- Load \"" + DICTIONARY_NAME + "\"\t2- Print size of the Dictionary\n"
+        "3- Insert Word             \t4- Look-up a Word\n"
+        "5- Print Tree Height       \t6- Print Black Height of the Tree\n"
+        "7- Exit\n"
+        "> ")
 
+    if option == '1':
+        readFile(DICTIONARY_NAME + ".txt")
+        print(DICTIONARY_NAME + " loaded successfully!")
+
+    elif option == '2':
+        print(DICTIONARY_NAME + ' currently has ' + str(tree.number_of_nodes) + ' words!')
+
+    elif option == '3':
+        s = str(input("Enter the word you want to insert: ")).strip()
+        if tree.search(s.lower()):
+            print("\"" + s + "\" is already in the dictionary!")
+        elif len(s) > 0 and not s.isspace():
+            tree.insert(s.lower())
+            print('\"' + s + '\" inserted Successfully')
+        else:
+            print('Invalid entry')
+
+    elif option == '4':
+        s = str(input("Enter the word you want to look-up: ")).strip()
+        if tree.search(s.lower()):
+            print("FOUND \"" + s + '\"!')
+        else:
+            print("\"" + s + '\" DOES NOT EXIST IN THE DICTIONARY')
+
+    elif option == '5':
+        print(tree.heightOfTree(tree.root, 0))
+
+    elif option == '6':
+        print(tree.getBlackHeight())
+
+    elif option == '7':
+        print("Thank you for using our application! :)")
+        break
+
+    print()
